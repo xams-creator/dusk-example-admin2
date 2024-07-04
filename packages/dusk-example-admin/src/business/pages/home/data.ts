0ce1b846074eafc65b1b727aeba7c62c596a2b0d
xams-creator <1228552328@qq.com>
2024-07-04 17:55:02 +0800

    feat: 初始化项目

--- packages/dusk-example-admin/src/business/pages/home/data.ts
+++ packages/dusk-example-admin/src/business/pages/home/data.ts
@@ -1,26 +1,4 @@
 export const data = [
-    // {
-    //     id: '1',
-    //     name: '目录1',
-    //     isLeaf: false,
-    //     parentId: '0',
-    // },
-    // {
-    //     id: '1-101',
-    //     name: '订单',
-    //     isLeaf: true,
-    //     tid: 'ttx.wso.bill',
-    //     oid: 'order',
-    //     parentId: '1',
-    // },
-    // {
-    //     id: '1-102',
-    //     name: '入库单',
-    //     isLeaf: true,
-    //     tid: 'ttx.wso.bill',
-    //     oid: 'receipt_header',
-    //     parentId: '1',
-    // },
     {
         id: 'router',
         name: '基座',
@@ -75,104 +53,6 @@ export const data = [
         },
     },
     {
-        id: 'cbt-web-custom',
-        name: 'cbt-web-custom',
-        isLeaf: true,
-        parentId: 'outer',
-        micro: true,
-        props: {
-            micro: {
-                name: 'cbt-web-custom',
-                iframe: false,
-                url: 'http://localhost:8003',
-            },
-        },
-    },
-    {
-        id: 'cbt',
-        name: 'cbt',
-        isLeaf: false,
-        parentId: '0',
-    },
-    {
-        id: 'wms-remote',
-        name: 'wms-remote',
-        isLeaf: true,
-        parentId: 'cbt',
-        micro: true,
-        props: {
-            micro: {
-                name: 'wms-remote',
-                iframe: true,
-                url: 'http://localhost:8001',
-            },
-        },
-    },
-    {
-        id: 'wms-remote2',
-        name: 'wms-remote2',
-        isLeaf: true,
-        parentId: 'cbt',
-        micro: true,
-        props: {
-            micro: {
-                name: 'wms-remote2',
-                iframe: true,
-                url: 'http://localhost:8001/index.html',
-            },
-        },
-    },
-    {
-        id: 'wms-remote3',
-        name: 'wms-remote3',
-        isLeaf: true,
-        parentId: 'cbt',
-        micro: true,
-        props: {
-            micro: {
-                name: 'wms-remote3',
-                iframe: false,
-                inline: true,
-                url: 'http://localhost:8002',
-            },
-        },
-    },
-    {
-        id: 'studio',
-        name: 'cbt-studio',
-        isLeaf: true,
-        parentId: 'cbt',
-        micro: true,
-        props: {
-            micro: {
-                name: 'cbt-studio',
-                baseroute: '/apps/studio',
-                url: 'http://localhost:8004/cbt-studio',
-            },
-        },
-    },
-    {
-        id: 'dojo',
-        name: 'dojo',
-        isLeaf: false,
-        parentId: '0',
-    },
-    {
-        id: 'dojo-hello-world',
-        name: 'dojo hello world',
-        isLeaf: true,
-        parentId: 'dojo',
-        micro: true,
-        props: {
-            micro: {
-                name: 'dojo-hello-world',
-                url: 'http://localhost:1341/',
-                micro: true,
-                iframe: true,
-            },
-        },
-    },
-    {
         id: 'admin',
         isLeaf: false,
         parentId: '0',
@@ -215,15 +95,9 @@ export const data = [
         name: 'react',
     },
     {
-        id: 'dusk-react',
-        isLeaf: false,
-        parentId: '0',
-        name: 'react',
-    },
-    {
         id: 'dusk-react17-hash',
         isLeaf: true,
-        parentId: 'dusk-react',
+        parentId: 'react',
         micro: true,
         name: 'dusk-react17-hash',
         props: {
@@ -236,7 +110,7 @@ export const data = [
     {
         id: 'dusk-react17-browser',
         isLeaf: true,
-        parentId: 'dusk-react',
+        parentId: 'react',
         micro: true,
         name: 'dusk-react17-browser',
         props: {
